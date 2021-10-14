3b809e97b7fd75637221e9ecf8be33868c914952
Fikcup <rhys.n.wright@gmail.com>
2021-10-13 17:50:34 -0700

    feat : update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -33,6 +33,20 @@ const userController = {
                 res.status(500).json(err);
             });
     },
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true, }
+        )
+            .then((userData) => {
+                res.json(userData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
 };
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,12 +3,13 @@ const {
     getAllUsers,
     getOneUser,
     createUser,
+    updateUser,
 } = require('../../controllers/user-controller');
 
 // /api/users
 router.route('/').get(getAllUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getOneUser);
+router.route('/:userId').get(getOneUser).put(updateUser);
 
 module.exports = router;
