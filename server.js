238790c1732e7c052ef3973a845578e3c0d19e83
Angel Ancheta <angelancheta98@yahoo.com>
2021-10-16 12:44:36 -0700

    added templating engine

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// set the handlebars engine
+app.engine('.handlebars', exphbs({
+    defaultLayout: 'main',
+    extname: '.handlebars',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+  }));
+// tell the app what folder our views is going to be in  
+app.set('view engine', '.handlebars');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+// test route for the root route
+app.get("/", function(req,res){
+    res.render("all-wishes")
+})
+
 app.use(routes);
 
 db.once('open', () => {
