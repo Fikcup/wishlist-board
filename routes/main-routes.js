aabc34f73560dbcf50f1413903eb08f7d7b00c39
Fikcup <rhys.n.wright@gmail.com>
2021-11-05 11:23:20 -0700

    feat : login partial with jwt token auth

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
+const authMiddleware = require('../middleware/auth');
+
+router.get('/login', (req, res) => {
+    if (authMiddleware.jwtAuth) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('partials/login');
+})
 
 module.exports = router;
