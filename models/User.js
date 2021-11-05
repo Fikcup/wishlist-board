2363a2281c03f43b598cf259983183a4824a2789
Fikcup <rhys.n.wright@gmail.com>
2021-11-05 10:05:49 -0700

    fix : encrypted password will not display on get request

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
             require: true,
             minlength: 8,
             maxlength: 128,
-            
+            select: false,
         },
         userFirst: {
             type: String,
