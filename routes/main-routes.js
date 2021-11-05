92b1bf1792175d9c9516616cb2ff6dae1aa7a8c1
Fikcup <rhys.n.wright@gmail.com>
2021-11-05 11:29:19 -0700

    feat : signup partial with jwt token auth

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -8,6 +8,15 @@ router.get('/login', (req, res) => {
     }
 
     res.render('partials/login');
+});
+
+router.get('/signup', (req, res) => {
+    if (authMiddleware.jwtAuth) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('partials/signup');
 })
 
 module.exports = router;
